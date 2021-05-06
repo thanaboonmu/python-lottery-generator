f4b444d95669cc03c5cd136548d8bd3ed36d489b
Thanaboon Muangwong <thanaboon.mu@gmail.com>
2021-05-06 21:04:13 +1000

    commit first sampling (unfinish)

---
+++ lottery-gen.py
@@ -0,0 +1,13 @@
+import numpy
+import random
+import math
+def generate_lottery(type,count,exclude_numbers):
+    if type == "Ozlotto":
+        print("These winning 7 numbers, along with 2 supplementary numbers, are randomly selected from a pool of 45 balls, numbered 1 to 45")
+        for i in range(count):
+            result=random.sample()
+            print(result)
+    return 0
+
+if __name__ == 'main':
+    sys.argv[1]
